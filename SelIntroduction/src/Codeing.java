3e4a21dbeea53c9778b4ba964b2e5b73c5c5c40b
shekh salahuddin <shekhsalahuddin81@gmail.com>
2022-08-30 13:04:00 -0500

    push2

--- SelIntroduction/src/Codeing.java
+++ SelIntroduction/src/Codeing.java
@@ -2,7 +2,7 @@
 public class Codeing {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hello Java");
 		String s = "ABCD";
 		String revS ="";
 		for (int i=0;i<s.length();i++) {
