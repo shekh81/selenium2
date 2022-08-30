009d7648f6607ae8b3f8db70598af53d820a48ed
shekh salahuddin <shekhsalahuddin81@gmail.com>
2022-08-30 13:51:41 -0500

    length

--- SelIntroduction/src/Code1.java
+++ SelIntroduction/src/Code1.java
@@ -2,12 +2,9 @@
 public class Code1 {
 
 	public static void main(String[] args) {
-		//Reverse String
-		String s = "ABCD";
-		String revS = "";
-		for(int i = 0; i<s.length();i++) {
-		char c = s.charAt(i);
-		revS = c+revS;
-		}
+		//Length
+		int [] a = {1,2,3,4,5};
+		System.out.println(a.length);
+		
 	}
 }
